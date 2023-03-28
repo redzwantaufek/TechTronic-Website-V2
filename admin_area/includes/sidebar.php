4f659c51b7fbc00765632fd0c7d81a3f95ce6193
redzwantaufek <redzwantaufek2@gmail.com>
2023-03-28 13:14:56 +0800

    initial commit

--- admin_area/includes/sidebar.php
+++ admin_area/includes/sidebar.php
@@ -32,7 +32,9 @@ else {
 
 </button><!-- navbar-ex1-collapse Ends -->
 
-<a class="navbar-brand" href="index.php?dashboard" >Admin Panel</a>
+<a class="navbar-brand" href="index.php?dashboard" >
+    Admin Panel TechTronic Website
+</a>
 
 
 </div><!-- navbar-header Ends -->
@@ -164,87 +166,8 @@ else {
 
 </li><!-- Products li Ends -->
 
-<li><!-- Bundles Li Starts --->
-
-<a href="#" data-toggle="collapse" data-target="#bundles">
-
-<i class="fa fa-fw fa-edit"></i> Bundles
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a>
-
-<ul id="bundles" class="collapse">
-
-<li>
-<a href="index.php?insert_bundle"> Insert Bundle </a>
-</li>
-
-<li>
-<a href="index.php?view_bundles"> View Bundles </a>
-</li>
-
-</ul>
-
-</li><!-- Bundles Li Ends --->
-
-<li><!-- relations li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#relations"><!-- anchor Starts -->
-
-<i class="fa fa-fw fa-retweet"></i> Assign Products To Bundles Relations
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a><!-- anchor Ends -->
-
-<ul id="relations" class="collapse"><!-- collapse Starts -->
-
-<li>
-
-<a href="index.php?insert_rel"> Insert Relation </a>
-
-</li>
 
 
-<li>
-
-<a href="index.php?view_rel"> View Relations </a>
-
-</li>
-
-</ul><!-- collapse Ends -->
-
-
-</li><!-- relations li Ends -->
-
-
-
-<li><!-- manufacturer li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#manufacturers"><!-- anchor Starts -->
-
-<i class="fa fa-fw fa-briefcase"></i> Manufacturers
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-
-</a><!-- anchor Ends -->
-
-<ul id="manufacturers" class="collapse"><!-- ul collapse Starts -->
-
-<li>
-<a href="index.php?insert_manufacturer"> Insert Manufacturer </a>
-</li>
-
-<li>
-<a href="index.php?view_manufacturers"> View Manufacturers </a>
-</li>
-
-</ul><!-- ul collapse Ends -->
-
-
-</li><!-- manufacturer li Ends -->
 
 
 <li><!-- li Starts -->
@@ -274,153 +197,6 @@ else {
 </li><!-- li Ends -->
 
 
-<li><!-- li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#cat">
-
-<i class="fa fa-fw fa-arrows-v"></i> Categories
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a>
-
-<ul id="cat" class="collapse">
-
-<li>
-<a href="index.php?insert_cat"> Insert Category </a>
-</li>
-
-<li>
-<a href="index.php?view_cats"> View Categories </a>
-</li>
-
-
-</ul>
-
-</li><!-- li Ends -->
-
-
-
-<li><!-- store section li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#store">
-
-<i class="fa fa-fw fa-briefcase"></i> Stores
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a>
-
-<ul id="store" class="collapse">
-
-<li>
-<a href="index.php?insert_store"> Insert store </a>
-</li>
-
-<li>
-<a href="index.php?view_store"> View store </a>
-</li>
-
-</ul>
-
-</li><!-- store section li Ends -->
-
-
-<li><!-- contact us li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#contact_us"><!-- anchor Starts -->
-
-<i class="fa fa-fw fa-pencil"> </i> Contact Us Section
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a><!-- anchor Ends -->
-
-<ul id="contact_us" class="collapse">
-
-<li>
-
-<a href="index.php?edit_contact_us"> Edit Contact Us </a>
-
-</li>
-
-<li>
-
-<a href="index.php?insert_enquiry"> Insert Enquiry Type </a>
-
-</li>
-
-<li>
-
-<a href="index.php?view_enquiry"> View Enquiry Types </a>
-
-</li>
-
-</ul>
-
-</li><!-- contact us li Ends -->
-
-<li><!-- about us li Starts -->
-
-<a href="index.php?edit_about_us">
-
-<i class="fa fa-fw fa-edit"></i> Edit About Us Page
-
-</a>
-
-</li><!-- about us li Ends -->
-
-
-<li><!-- Coupons Section li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#coupons"><!-- anchor Starts -->
-
-<i class="fa fa-fw fa-arrows-v"></i> Coupons
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a><!-- anchor Ends -->
-
-<ul id="coupons" class="collapse"><!-- ul collapse Starts -->
-
-<li>
-<a href="index.php?insert_coupon"> Insert Coupon </a>
-</li>
-
-<li>
-<a href="index.php?view_coupons"> View Coupons </a>
-</li>
-
-</ul><!-- ul collapse Ends -->
-
-</li><!-- Coupons Section li Ends -->
-
-
-
-<li><!-- terms li Starts -->
-
-<a href="#" data-toggle="collapse" data-target="#terms" ><!-- anchor Starts -->
-
-<i class="fa fa-fw fa-table"></i> Terms
-
-<i class="fa fa-fw fa-caret-down"></i>
-
-</a><!-- anchor Ends -->
-
-<ul id="terms" class="collapse"><!-- ul collapse Starts -->
-
-<li>
-<a href="index.php?insert_term"> Insert Terms </a> 
-</li>
-
-<li>
-<a href="index.php?view_terms"> View Terms </a> 
-</li>
-
-</ul><!-- ul collapse Ends -->
-
-
-</li><!-- terms li Ends -->
 
 
 
