393b9df9d595550ba4eb7453fbdb89a34e6e063c
redzwantaufek <redzwantaufek2@gmail.com>
2023-03-29 22:24:08 +0800

    initial commit

--- payment_options.php
+++ payment_options.php
@@ -25,12 +25,7 @@ $customer_id = $row_customer['customer_id'];
 
 <center><!-- center Starts -->
 
-  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-  <input type="hidden" name="cmd" value="_s-xclick">
-  <input type="hidden" name="hosted_button_id" value="9PWJZYVQH8KGU">
-  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-  <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-  </form>
+ 
 
 
 <?php
@@ -76,7 +71,6 @@ $i++;
 
 <?php } ?>
 
-<input type="image" name="submit" width="500" height="270" src="images/paypal.png" >
 
 
 </form><!-- form Ends -->
